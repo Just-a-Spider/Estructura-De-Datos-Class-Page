596e02848a88975d8c61d343067f9cf74663edc0
Just-a-Spider <andre_arg_0116@outlook.com>
2023-08-28 13:45:23 -0500

    Added Exercises and Style

--- EDPage/EDPage/settings.py
+++ EDPage/EDPage/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-l$3rjv0!%u39i!q)vc8imyeoyz83l(2%d=5!e^%$vvl93isaw-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','estructura.serveo.net']
+ALLOWED_HOSTS = ['127.0.0.1','estructura.serveo.net', '.ngrok.io', '.ngrok-free.app']
 
 
 # Application definition
