0f46f00c3a66ccaa14d2fc104958ec38433998da
Just-a-Spider <andre_arg_0116@outlook.com>
2023-11-06 17:12:35 -0500

    Refactor URL configuration and views

--- EDPage/EDPage/urls.py
+++ EDPage/EDPage/urls.py
@@ -1,34 +1,18 @@
-"""
-URL configuration for EDPage project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
 from exercises.views import *
 
 urlpatterns = [
-    path('', differences, name='inicio'),
-    path('datos/', types, name='tipos'),
-    path('datos/<str:type_name>/', type_detail, name='tipos_detail'),
-    path('diferencias/', differences, name='diferencia'),
-    path('diferencias/<str:method_name>', differences_view, name = 'diferencias_detail' ),
-    path('temas/', topics_view, name='temas'),
-    path('temas/<str:category_name>/', category_detail, name='categoria'),
-    path('ejercicios/', exercises_view, name='ejercicios'),
-    path('ejercicios/<str:category_name>/', exercise_list, name='lista'),
-    path('ejercicios/<str:category_name>/<str:exer_title>/', exercise_detail, name='ejemplo'),
+    path('', DifferencesView.as_view(), name='inicio'),
+    path('datos/', TypesView.as_view(), name='tipos'),
+    path('datos/<str:type_name>/', TypeDetailView.as_view(), name='tipos_detail'),
+    path('diferencias/', DifferencesView.as_view(), name='diferencia'),
+    path('diferencias/<str:method_name>', DifferencesDetailView.as_view(), name = 'diferencias_detail' ),
+    path('temas/', CategoriesView.as_view(), name='temas'),
+    path('temas/<str:category_name>/', CategoryDetailView.as_view(), name='categoria'),
+    path('ejercicios/', ExercisesView.as_view(), name='ejercicios'),
+    path('ejercicios/<str:category_name>/', ExerciseListView.as_view(), name='lista'),
+    path('ejercicios/<str:category_name>/<str:exer_title>/', ExerciseDetailView.as_view(), name='ejemplo'),
     path('admin/', admin.site.urls),
 ]
--- EDPage/exercises/views.py
+++ EDPage/exercises/views.py
@@ -1,79 +1,81 @@
 import random
 from django.shortcuts import render, get_object_or_404
+from django.views import View
 from .models import Categories, Exercises, Methods, Types
-#-------------------------------------------------------------------------------
-# Extract elements of the models.descriptions
 import re
+
+# ------------------- Helper functions -------------------
+# To get a content list based on the description
 def parse_description(description):
-    # Split the description into paragraphs
     paragraphs = description.split('\n')
-    # Initialize variables to store the content and list elements
     content_element = ""
     list_element = []
-    # Iterate through the paragraphs
     for paragraph in paragraphs:
-        # Check if the paragraph starts with a digit followed by a period
         if re.match(r'^\d+\.\s', paragraph):
             list_element.append(paragraph)
         else:
             content_element += paragraph + "\n"
     return content_element.strip(), list_element
-#-------------------------------------------------------------------------------
-# Home Views.
-def home(request):
-    return render(request, 'home.html')
-#-------------------------------------------------------------------------------
-# Types Views.
-def types(request):
-    type_list = Types.objects.all()
-    return render(request, 'types.html',
-                  {'tipos': type_list})
-def type_detail(request, type_name):
-    type = get_object_or_404(Types, name=type_name)
-    return render(request, 'type_detail.html',
-                  {'type': type})
-#-------------------------------------------------------------------------------
-# Differences Views.
-def differences(request):
-    method_list = Methods.objects.all()
-    return render(request, "differences.html", 
-                  {'methods':method_list})
-def differences_view(request, method_name):
-    method = get_object_or_404(Methods, func=method_name)
-    return render(request, 'differences_detail.html',
-                  {'method': method})
-#-------------------------------------------------------------------------------
-# Themes Views.
-def topics_view(request):
-    categories_list = Categories.objects.all()
-    return render(request, 'categories.html', 
-                  {'categories': categories_list})
-
-def category_detail(request, category_name):
+# To get a category and exercise based on the name
+def get_category_and_exercise(category_name, exer_title=None):
     category = get_object_or_404(Categories, name=category_name)
-    exercise = Exercises.objects.filter(category=category)
-    random_exercise = random.choice(exercise)
-    textExam, objectives = parse_description(random_exercise.desc)
-    textCat, steps = parse_description(category.content)
-    return render(request, 'category_detail.html',
-                  {'category': category, 'exercise': random_exercise,'textCat':textCat, 'steps':steps, 
-                   'text':textExam, 'objectives':objectives})
-#--------------------------------------------------------------------------------
-# Exercises Views
-def exercises_view(request):
-    categories_list = Categories.objects.all()
-    return render(request, "exercises.html", 
-                  {'categories': categories_list})
+    if exer_title:
+        exercise = get_object_or_404(Exercises, title=exer_title)
+        return category, exercise
+    return category, None
 
-def exercise_list(request, category_name):
-    category = get_object_or_404(Categories, name=category_name)
-    exercise = Exercises.objects.filter(category=category)
-    return render(request, 'exercise_list.html', 
-                  {'category': category, 'exercise': exercise})
-
-def exercise_detail(request, category_name, exer_title):
-    exercise = get_object_or_404(Exercises, title= exer_title)
-    text, objectives = parse_description(exercise.desc)
-    return render(request, 'exercise_detail.html', 
-                  {'category':category_name,'exercise':exercise, 'text':text, 'objectives':objectives})
-#--------------------------------------------------------------------------------
+# ------------------- Views -------------------
+# Home page
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+#----------------------------------------------
+# Types
+class TypesView(View):
+    def get(self, request):
+        type_list = Types.objects.all()
+        return render(request, 'types.html', {'tipos': type_list})
+class TypeDetailView(View):
+    def get(self, request, type_name):
+        type = get_object_or_404(Types, name=type_name)
+        return render(request, 'type_detail.html', {'type': type})
+#----------------------------------------------
+# Differences
+class DifferencesView(View):
+    def get(self, request):
+        method_list = Methods.objects.all()
+        return render(request, "differences.html", {'methods': method_list})
+class DifferencesDetailView(View):
+    def get(self, request, method_name):
+        method = get_object_or_404(Methods, func=method_name)
+        return render(request, 'differences_detail.html', {'method': method})
+#----------------------------------------------
+# Categories
+class CategoriesView(View):
+    def get(self, request):
+        categories_list = Categories.objects.all()
+        return render(request, 'categories.html', {'categories': categories_list})
+class CategoryDetailView(View):
+    def get(self, request, category_name):
+        category, _ = get_category_and_exercise(category_name)
+        exercise = Exercises.objects.filter(category=category)
+        random_exercise = random.choice(exercise)
+        textExam, objectives = parse_description(random_exercise.desc)
+        textCat, steps = parse_description(category.content)
+        return render(request, 'category_detail.html', {'category': category, 'exercise': random_exercise,'textCat':textCat, 'steps':steps, 'text':textExam, 'objectives':objectives})
+#----------------------------------------------
+# Exercises
+class ExercisesView(View):
+    def get(self, request):
+        categories_list = Categories.objects.all()
+        return render(request, "exercises.html", {'categories': categories_list})
+class ExerciseListView(View):
+    def get(self, request, category_name):
+        category, _ = get_category_and_exercise(category_name)
+        exercise = Exercises.objects.filter(category=category)
+        return render(request, 'exercise_list.html', {'category': category, 'exercise': exercise})
+class ExerciseDetailView(View):
+    def get(self, request, category_name, exer_title):
+        category, exercise = get_category_and_exercise(category_name, exer_title)
+        text, objectives = parse_description(exercise.desc)
+        return render(request, 'exercise_detail.html', {'category':category_name,'exercise':exercise, 'text':text, 'objectives':objectives})
