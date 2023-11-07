8994f74f4eb40d2f48d7a41ed581c0e9340b0518
Just-a-Spider <andre_arg_0116@outlook.com>
2023-11-07 17:39:09 -0500

    Refactor asgi.py to simplify code

--- EDPage/EDPage/asgi.py
+++ EDPage/EDPage/asgi.py
@@ -1,13 +1,7 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.utils.module_loading import import_string
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EDPage.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": URLRouter(
-        import_string('exercises.routing.websocket_urlpatterns')
-    ),
-})
+application = get_asgi_application()
