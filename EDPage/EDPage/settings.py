191aca0387278b64ec097abbca718f66eda43bc8
Just-a-Spider <andre_arg_0116@outlook.com>
2023-11-10 08:50:08 -0500

    Changed database engine to PostgreSQL in settings.py

--- EDPage/EDPage/settings.py
+++ EDPage/EDPage/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'EDPage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'EDPageDB',
+        'USER': 'postgres',
+        'PASSWORD': '0116',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
