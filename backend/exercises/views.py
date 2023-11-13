8986f82a698853aa24bc4f06b15e3ed2b5b8bdc7
Just-a-Spider <andre_arg_0116@outlook.com>
2023-11-13 18:22:29 -0500

    Changed the all-View class approach Into a generic-view classes

--- backend/exercises/views.py
+++ backend/exercises/views.py
@@ -2,10 +2,10 @@ import random
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Categories, Exercises, Methods, Types, Subcategories, Subtypes
+from django.views.generic import ListView, DetailView
 import re
 
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 import subprocess
 import pexpect
 # ------------------- Helper functions -------------------
@@ -20,6 +20,7 @@ def parse_description(description):
         else:
             content_element += paragraph + "\n"
     return content_element.strip(), list_element
+
 # To get a category and exercise based on the name
 def get_category_and_exercise(category_name, exer_title=None):
     category = get_object_or_404(Categories, name=category_name)
@@ -27,6 +28,7 @@ def get_category_and_exercise(category_name, exer_title=None):
         exercise = get_object_or_404(Exercises, title=exer_title)
         return category, exercise
     return category, None
+
 # To get the code running
 def execute_code(code, language, input_data):
     match language:
@@ -54,7 +56,7 @@ def execute_code(code, language, input_data):
                 return child.before.decode()  # Return the output of the script
         except pexpect.TIMEOUT:  # If the script doesn't output anything for a while
             return 'Input required'  # Return 'Input required'
-@csrf_exempt
+        
 def execute_code_view(request):
     if request.method == 'POST':
         code = request.POST.get('code')
@@ -74,85 +76,143 @@ class HomeView(View):
         return render(request, 'index.html')
 #----------------------------------------------
 # Types
-class TypesView(View):
-    def get(self, request):
-        type_list = Types.objects.all()
-        return render(request, 'types.html', {'tipos': type_list})
-class TypeDetailView(View):
-    def get(self, request, type_name):
-        type = get_object_or_404(Types, name=type_name)
-        subtypes = Subtypes.objects.filter(type=type)
-        return render(request, 'type_detail.html', {'type': type, 
-                                                    'subtypes': subtypes})
-class SubTypeDetailView(View):
-    def get(self, request, type_name, subtype_name):
-        type = get_object_or_404(Types, name=type_name)
-        subtype = get_object_or_404(Subtypes, name=subtype_name)
-        return render(request, 'subtype_detail.html', {'type': type, 
-                                                       'subtype': subtype})
+class TypesView(ListView):
+    model = Types
+    template_name = "types.html"
+    context_object_name = 'tipos'
+    
+class TypeDetailView(DetailView):
+    model = Types
+    template_name = 'type_detail.html'
+    context_object_name = 'type'
+    slug_field = 'name'
+    slug_url_kwarg = 'type_name'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        subtypes = Subtypes.objects.filter(type=self.object)
+        context.update({
+            'subtypes': subtypes
+        })
+        return context
+
+class SubTypeDetailView(DetailView):
+    model = Subtypes
+    template_name = 'subtype_detail.html'
+    context_object_name = 'subtype'
+    slug_field = 'name'
+    slug_url_kwarg = 'subtype_name'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        type = Types.objects.get(name=self.kwargs['type_name'])
+        context.update({
+            'type': type
+        })
+        return context
 #----------------------------------------------
 # Differences
-class DifferencesView(View):
-    def get(self, request):
-        method_list = Methods.objects.all()
-        return render(request, "differences.html", {'methods': method_list})
-class DifferencesDetailView(View):
-    def get(self, request, method_name):
-        method = get_object_or_404(Methods, func=method_name)
-        return render(request, 'differences_detail.html', {'method': method})
+class DifferencesView(ListView):
+    model = Methods
+    template_name = "differences.html"
+    context_object_name = 'methods'
+
+class DifferencesDetailView(DetailView):
+    model = Methods
+    template_name = 'differences_detail.html'
+    context_object_name = 'method'
+    slug_field = 'func'
+    slug_url_kwarg = 'method_name'
 #----------------------------------------------
 # Categories
-class CategoriesView(View):
-    def get(self, request):
-        categories_list = Categories.objects.all()
-        return render(request, 'categories.html', {'categories': categories_list})
-class CategoryDetailView(View):
-    def get(self, request, category_name):
-        category, _ = get_category_and_exercise(category_name)
-        exercise = Exercises.objects.filter(category=category)
-        random_exercise = random.choice(exercise)
+class CategoriesView(ListView):
+    model = Categories
+    template_name = "categories.html"
+    context_object_name = 'categories'
+
+class CategoryDetailView(DetailView):
+    model = Categories
+    template_name = 'category_detail.html'
+    context_object_name = 'category'
+    slug_field = 'name'
+    slug_url_kwarg = 'category_name'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        exercise = Exercises.objects.filter(category=self.object)
+        random_exercise = random.choice(list(exercise))
         textExam, objectives = parse_description(random_exercise.desc)
-        textCat, steps = parse_description(category.content)
-        subcategories = Subcategories.objects.filter(category=category)
-        return render(request, 'category_detail.html', {'category': category, 
-                                                        'exercise': random_exercise,
-                                                        'textCat':textCat, 
-                                                        'steps':steps, 
-                                                        'text':textExam, 
-                                                        'objectives':objectives,
-                                                        'subcategories': subcategories})
-class SubcategoriesDetailView(View):
-    def get(self, request, category_name, subcategory_name):
-        category, _ = get_category_and_exercise(category_name)
-        subcategory = get_object_or_404(Subcategories, name=subcategory_name)
-        exercise = Exercises.objects.filter(category=category, subcategoty=subcategory)
-        random_exercise = random.choice(exercise)
+        textCat, steps = parse_description(self.object.content)
+        subcategories = Subcategories.objects.filter(category=self.object)
+        context.update({
+            'exercise': random_exercise,
+            'textCat': textCat,
+            'steps': steps,
+            'text': textExam,
+            'objectives': objectives,
+            'subcategories': subcategories
+        })
+        return context
+    
+class SubcategoriesDetailView(DetailView):
+    model = Subcategories
+    template_name = 'subcategory_detail.html'
+    context_object_name = 'subcategory'
+    slug_field = 'name'
+    slug_url_kwarg = 'subcategory_name'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category, _ = get_category_and_exercise(self.kwargs['category_name'])
+        exercise = Exercises.objects.filter(category=category, subcategoty=self.object)
+        random_exercise = random.choice(list(exercise))
         textExam, objectives = parse_description(random_exercise.desc)
-        textCat, steps = parse_description(subcategory.content)
-        return render(request, 'subcategory_detail.html', {'category': category, 
-                                                           'subcategory': subcategory, 
-                                                           'exercise': random_exercise,
-                                                           'textCat':textCat, 
-                                                           'steps':steps, 
-                                                           'text':textExam, 
-                                                           'objectives':objectives})
+        textCat, steps = parse_description(self.object.content)
+        context.update({
+            'category': category,
+            'exercise': random_exercise,
+            'textCat': textCat,
+            'steps': steps,
+            'text': textExam,
+            'objectives': objectives
+        })
+        return context
 #----------------------------------------------
 # Exercises
-class ExercisesView(View):
-    def get(self, request):
-        categories_list = Categories.objects.all()
-        return render(request, "exercises.html", {'categories': categories_list})
-class ExerciseListView(View):
-    def get(self, request, category_name):
-        category, _ = get_category_and_exercise(category_name)
-        exercise = Exercises.objects.filter(category=category)
-        return render(request, 'exercise_list.html', {'category': category, 
-                                                      'exercise': exercise})
-class ExerciseDetailView(View):
-    def get(self, request, category_name, exer_title):
-        category, exercise = get_category_and_exercise(category_name, exer_title)
-        text, objectives = parse_description(exercise.desc)
-        return render(request, 'exercise_detail.html', {'category':category_name,
-                                                        'exercise':exercise, 
-                                                        'text':text, 
-                                                        'objectives':objectives})
+class ExercisesView(ListView):
+    model = Categories
+    template_name = "exercises.html"
+    context_object_name = 'categories'
+
+class ExerciseListView(ListView):
+    model = Exercises
+    template_name = 'exercise_list.html'
+    context_object_name = 'exercise'
+
+    def get_queryset(self):
+        self.category, _ = get_category_and_exercise(self.kwargs['category_name'])
+        return Exercises.objects.filter(category=self.category)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
+
+class ExerciseDetailView(DetailView):
+    model = Exercises
+    template_name = 'exercise_detail.html'
+    context_object_name = 'exercise'
+
+    def get_object(self):
+        category, exercise = get_category_and_exercise(self.kwargs['category_name'], self.kwargs['exer_title'])
+        return exercise
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        text, objectives = parse_description(self.object.desc)
+        context.update({
+            'category': self.kwargs['category_name'],
+            'text': text,
+            'objectives': objectives
+        })
+        return context
