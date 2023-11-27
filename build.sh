5d72b3287b9bfee8ddf6c768ff92ff1f7fd1acb9
Just-a-Spider <andre_arg_0116@outlook.com>
2023-11-27 17:18:27 -0500

    Flushing the data

--- build.sh
+++ build.sh
@@ -9,4 +9,5 @@ pip install -r requirements.txt
 python backend/manage.py makemigrations exercises 
 python backend/manage.py migrate
 python backend/manage.py collectstatic --noinput
+python backend/manage.py flush --no-input
 python backend/manage.py loaddata whole.json
