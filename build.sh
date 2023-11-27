c97f3df1009d85e6a822505a859e80a34d93c1bc
Just-a-Spider <andre_arg_0116@outlook.com>
2023-11-27 16:15:19 -0500

    Remove loaddata

--- build.sh
+++ build.sh
@@ -6,6 +6,5 @@ set -o errexit
 pip install -r requirements.txt
 
 # run the application
-python backend/manage.py makemigrations
+python backend/manage.py makemigrations exercises 
 python backend/manage.py migrate
-python backend/manage.py loaddata whole.json
